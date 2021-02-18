728261f6c8cbf06961189516b419203410afad50
Karl Buys <kbbuys@gmail.com>
2021-02-18 16:10:14 +0200

    Check validity of mnemonic before attempting to save it

--- local_modules/WalletWizard/Views/UseExisting_MetaInfo_View.web.js
+++ local_modules/WalletWizard/Views/UseExisting_MetaInfo_View.web.js
@@ -548,6 +548,16 @@ class UseExisting_MetaInfo_View extends BaseView_Wallet_MetaInfo
 				window.plugins.insomnia.keepAwake() // disable screen dim/off
 			}
 			//
+			try {
+				let ret = self.context.monero_utils.seed_and_keys_from_mnemonic(
+					self.lookup__mnemonicSeed(),
+					self.context.nettype
+				);
+			} catch (e) {
+				console.error("Invalid mnemonic!");
+				__trampolineFor_failedWithErrStr(e);
+				return
+			} 
 			self.validationMessageLayer.ClearAndHideMessage()
 			//
 			self.rightBarButtonView.layer.innerHTML = commonComponents_activityIndicators.New_Graphic_ActivityIndicatorLayer_htmlString({"margin-top": "3px"})
