772efa71c3d4cba45c6e3569027283312a14ed90
Karl Buys <kbbuys@gmail.com>
2021-10-05 11:55:00 +0200

    build: copy local_modules content into browser_build when using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: "node_modules/@mymonero/mymonero-app-bridge/MyMoneroLibAppCpp_WASM.js", to: "../browser_build/MoneroLibAppCpp_WASM.js" },
-        { from: "node_modules/@mymonero/mymonero-app-bridge/MyMoneroLibAppCpp_WASM.wasm", to: "../browser_build/assets/MyMoneroLibAppCpp_WASM.wasm" }
+        { from: "node_modules/@mymonero/mymonero-app-bridge/MyMoneroLibAppCpp_WASM.wasm", to: "../browser_build/assets/MyMoneroLibAppCpp_WASM.wasm" },
+        { from: 'local_modules', to: '../browser_build' },
       ]
     })
   ],
