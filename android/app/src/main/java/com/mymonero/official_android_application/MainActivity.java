57c974033939c8881356a6dfcfd32ad08bfa81eb
Karl Buys <kbbuys@gmail.com>
2022-03-09 14:14:57 +0200

    fix: remove manual plugin initialization from Java

--- android/app/src/main/java/com/mymonero/official_android_application/MainActivity.java
+++ android/app/src/main/java/com/mymonero/official_android_application/MainActivity.java
@@ -15,14 +15,5 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(SecureStoragePlugin.class);
-//      add(FileSelector.class);
-      add(CapacitorQRScanner.class);
-    }});
   }
 }
